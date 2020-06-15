c0e303dee40900a3392120ad9c5f3895d6d865d7
AvengerEug <eugenesumarry@163.com>
2020-06-15 17:22:54 +0800

    leetcode82题：去除链表中的重复元素

--- src/main/java/leetcode/DeleteDuplicates.java
+++ src/main/java/leetcode/DeleteDuplicates.java
@@ -1,9 +1,9 @@
 package leetcode;
 
 import leetcode.model.ListNode;
+import leetcode.model.TreeNode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
@@ -17,12 +17,69 @@ import java.util.Set;
  * 输出: 2->3
  *
  * 方法一：
- *   使用一个set记录去重的元素，遍历一个节点时，把元素放入set中，如果当前进入的元素是在set中
- *   ，那么则把root置为上一个元素
+ *   最简单的方案：使用hash表，key为node的值，value为出现的次数
+ *
+ *   遍历完链表后，再将hash表中value为1的节点按顺序组成个链表
+ *
  */
 public class DeleteDuplicates {
 
 
+    /**
+     * 最简单的方案：使用hash表，key为node的值，value为出现的次数
+     * 遍历完链表后，再将hash表中value为1的节点按顺序组成个链表
+     *
+     * 使用此种方式效率很低，但不可否认它是最容易想到的。
+     * @param root
+     * @return
+     */
+    public static ListNode deleteDuplForHash(ListNode root) {
+        Map<Integer, Integer> hash = new HashMap<>();
+
+        while (root != null) {
+            if (hash.containsKey(root.val)) {
+                int val = hash.get(root.val);
+                hash.put(root.val, ++val);
+            } else {
+                hash.put(root.val, 1);
+            }
+            root = root.next;
+        }
+
+        ListNode prevNode = new ListNode(-1);
+        ListNode tmp = prevNode;
+        Iterator<Integer> iterator = hash.keySet().iterator();
+        while (iterator.hasNext()) {
+            int key = iterator.next();
+            if (hash.get(key) == 1) {
+                ListNode newNode = new ListNode(key);
+                tmp.next = newNode;
+                tmp = newNode;
+            }
+        }
+
+        return prevNode.next;
+    }
+
+
+    /**
+     * ListNode container = new ListNode(-1);
+     * ListNode head = container;
+     * ListNode preNode = null;
+     *
+     * prevNode    head   newNode
+     *   null       -1      null
+     *    -1         1       1
+     *    1          2       2
+     *    2          3       3
+     *    2          3       3 ---- continue
+     *    2          4       4 ---- continue
+     *    2          4       4 ---- continue
+     *    2          5       5
+     *
+     * @param root
+     * @return
+     */
     public static ListNode deleteDupl(ListNode root) {
         if (root == null) {
             return null;
@@ -32,53 +89,53 @@ public class DeleteDuplicates {
             return root;
         }
 
-        ListNode tmp = root;
-        ListNode head = new ListNode(-1);
-        ListNode preNode = head;
+        // 使用root.val来创建head指针，防止出现元素都相同的链表
+        ListNode container = new ListNode(root.val);
+        ListNode head = container;
+        ListNode preNode = container;
 
-        int duplCount = 0;
+        boolean isDupl = false;
 
-        while (tmp != null) {
-            if (head.val != tmp.val) {
-                if (duplCount > 0) {
-                    head = preNode;
-                    head.next = tmp;
-                    duplCount = 0;
-                } else {
-                    head.next = tmp;
+        while (root != null) {
+            if (head.val != root.val) {
+                if (!isDupl) {
                     preNode = head;
-                    head = tmp;
                 }
+
+                head = root;
+                preNode.next = head;
+
+                isDupl = false;
             } else {
-                duplCount++;
+                isDupl = true;
             }
 
-            tmp = tmp.next;
+            root = root.next;
         }
 
-        return head.next;
+        return container.next;
     }
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
+        ListNode node2 = new ListNode(1);
         node1.next = node2;
 
-        ListNode node3 = new ListNode(3);
+        ListNode node3 = new ListNode(1);
         node2.next = node3;
 
-        ListNode node33 = new ListNode(3);
+        ListNode node33 = new ListNode(2);
         node3.next = node33;
 
-        ListNode node4 = new ListNode(4);
+        ListNode node4 = new ListNode(2);
         node33.next = node4;
 
-        ListNode node44 = new ListNode(4);
+        ListNode node44 = new ListNode(3);
         node4.next = node44;
 
-        ListNode node55 = new ListNode(5);
+        ListNode node55 = new ListNode(4);
         node44.next = node55;
 
-        System.out.println(deleteDupl(node1));
+        System.out.println(deleteDuplForHash(node1));
     }
 }
