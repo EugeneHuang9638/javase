705fd154a70ff8c4e986357ddc0e4fed47eb3cc4
avengerEug <eugenesumarry@163.com>
2020-12-03 21:47:11 +0800

    新增nio byteBuffer优化总结

--- src/main/java/io/nio/bytebuffer/ByteBufferTest.java
+++ src/main/java/io/nio/bytebuffer/ByteBufferTest.java
@@ -14,12 +14,37 @@ import java.nio.ByteBuffer;
  *
  * 通过get和put api来对byteBuffer的数据进行读取和填充
  *
+ * clear和flip的区别
+ *
+ * clear源码：
+ * position = 0;
+ * limit = capacity;
+ * mark = -1;
+ * return this;
+ *
+ *
+ * flip源码：
+ * limit = position;
+ * position = 0;
+ * mark = -1;
+ * return this;
+ *
+ * 可以看到区别就在于：limit的赋值
+ * 在clear中，limit的值变成了capacity的值  --> limit变成capacity的值，若强行读取，可能会读取出空数据
+ * 在flip中，limit的值变成了position的值   --> 实际上可以读取数据的长度
+ *
+ * 假设当前的limit = position = capacity  调用clear或flip所达到的效果是一样的
+ * 假设当前的limit > position,   调用clear和flip的区别只有在读取操作时才有所体现，
+ * 在clear后去读取数据，此时读取出来的数据包含空数据(limit 到 capacity的数据都会读取到)，
+ * 在flip后去读取数据，此时读取的数据是真实的数据长度
+ *
+ *
  */
 public class ByteBufferTest {
 
 
     public static void main(String[] args) {
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(12);
 
         // 看一下初始状态的四个属性的值
         System.out.println("初始状态下四个属性的值：");
@@ -34,6 +59,14 @@ public class ByteBufferTest {
         byteBuffer.put(bytes);
         System.out.println("avengerEug字符串转成byte数组后的长度为：" + bytes.length);
 
+        // ===> 此时在写两个字符进去的话，position会变成12，可以想象下，数组的长度为12，它的下标最大才11，如果下次操作下标为12的位置，那肯定会报错
+//        bytes = "eu".getBytes();
+//        byteBuffer.put(bytes);
+
+        // ===> 结合上述写入"eu"字符的情况，由于position变成12了，此时若进行写数据，肯定直接报错
+//        bytes = "g".getBytes();
+//        byteBuffer.put(bytes);
+
         System.out.println("填充字符串后四个属性的值：");
         System.out.println("limit: ==> " + byteBuffer.limit());
         System.out.println("position: ==> " + byteBuffer.position()); // ===> 正在操作的位置变成了10，是因为avengerEug这个字符串转成byte数组后的长度为10
@@ -45,17 +78,19 @@ public class ByteBufferTest {
         System.out.println("从byteBuffer中读取我们刚刚放进的数据，调用flip方法切换成读模式");
         // 要从byteBuffer中读取数据时，需要调用flip方法切换成读模式
         byteBuffer.flip();
+        // 将position置为0后，再写数据，测试是否会覆盖 ==> 的确会覆盖，但同时也会修改position的值
+//        byteBuffer.put("xixi".getBytes());
         System.out.println("切换成读模式后四个属性的值：");
-        System.out.println("limit: ==> " + byteBuffer.limit()); // ==> limit由1024变成10了
+        System.out.println("limit: ==> " + byteBuffer.limit()); // ==> limit由12变成10了
         System.out.println("position: ==> " + byteBuffer.position()); // ==> position由10变成0了
         System.out.println("capacity: ==>" + byteBuffer.capacity());
         System.out.println("mark: ==>" + byteBuffer.mark());
-        System.out.println("limit由1024变成10了, position由10变成0了。\n 得出结论：当调用flip切换成读模式时，整个byteBuffer的限制大小仅仅为10。\n " +
+        System.out.println("limit由12变成10了, position由10变成0了。\n 得出结论：当调用flip切换成读模式时，整个byteBuffer的限制大小仅仅为10。\n " +
                 "这是符合条件的，因为我们的缓冲区此时只有10个长度的数据，即avengerEug字符串转化成byte数组的长度");
 
         System.out.println("开始读取数据....");
         // 传入一个字节数组给get方法，字节数组的长度就是limit的值，执行完get方法后，会将数据填充到传入的byte数组中
-        byte[] readByte = new byte[byteBuffer.limit()];
+        byte[] readByte = new byte[byteBuffer.limit() - byteBuffer.position()];
         byteBuffer.get(readByte);
         System.out.println("读取到的数据：" + new String(readByte));
         System.out.println("读取数据后的四个属性的值：");
@@ -65,11 +100,11 @@ public class ByteBufferTest {
         System.out.println("mark: ==>" + byteBuffer.mark());
 
         // 此时无法再继续写数据了，因为limit为10，position也为10了，已经达到界限了。
-        // byteBuffer.put("asdfsdaf".getBytes());  // ==> 抛异常：java.nio.BufferOverflowException
+//         byteBuffer.put("as".getBytes());  // ==> 抛异常：java.nio.BufferOverflowException
 
         System.out.println("-------------------分隔符--------------------");
         System.out.println("调用clear方法，重回写模式，但缓冲区的数据会被清空，我们获取不到avengerEug数据了");
-        // 重回写模式
+        // 重回写模式 --> 但要注意：缓存区的值还没有被删除，我依然可以读取缓存区的值
         byteBuffer.clear();
         System.out.println("重回写模式后的四个属性的值：");
         System.out.println("limit: ==> " + byteBuffer.limit());
@@ -78,6 +113,9 @@ public class ByteBufferTest {
         System.out.println("mark: ==>" + byteBuffer.mark());
         System.out.println("结论：当调用clear方法后，byteBuffer会被清空，相当于回到最原始的情况了");
 
-
+        // 即时切回成了写模式，缓存区的值依然可以读取。
+        readByte = new byte[byteBuffer.limit() - byteBuffer.position()];
+        byteBuffer.get(readByte);
+        System.out.println("读取到的数据：" + new String(readByte));
     }
 }
--- src/main/java/io/nio/channel/ChannelTest.java
+++ src/main/java/io/nio/channel/ChannelTest.java
@@ -19,7 +19,7 @@ import java.nio.channels.FileChannel;
  *
  *
  * 本案例测试：
- * 使用FileChannel配置缓冲区来实现文件的复制
+ * 使用FileChannel配置缓存区来实现文件的复制
  *
  */
 public class ChannelTest {
@@ -27,8 +27,8 @@ public class ChannelTest {
 
     public static void main(String[] args) throws Exception {
 
-//        copyFileByByteBuffer();
-        copyFileByChannel();
+        copyFileByByteBuffer();
+//        copyFileByChannel();
 
     }
 
@@ -43,17 +43,45 @@ public class ChannelTest {
     }
 
     private static void copyFileByByteBuffer() throws IOException {
+        // D:\demo.txt 文件内容就是：avengerEug
         FileChannel inChannel = new FileInputStream("D:\\demo.txt").getChannel();
         FileChannel outChannel = new FileOutputStream("D:\\tmp\\demo.txt").getChannel();
 
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-        // inChannel将文件流读取到byteBuffer缓冲区中，大小为1024位
+        // 每次最大读取10个长度
+        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+        // inChannel将文件流读取到byteBuffer缓存区中，大小为10位  --> 每一次循环从文件中读取10个字符
+        // 类似于调用了byteBuffer.put方法，往缓存区写数据
         while (inChannel.read(byteBuffer) != -1) {
-            // 将内容写到文件中去，因此要读取byteBuffer的数据，因此要切换成读模式
+            /**
+             * 要进行写文件，我们需要从buffer中读取内容并写到文件中去，因此首先要对buffer进行读取数据.
+             * 读取的数据内容是数组的那一部分内容呢？很简单，
+             * 需要执行flip方法，即切换成读模式，然后缓存区position到limit的位置就是当前循环读取到的内容
+             */
             byteBuffer.flip();
 
+            /**
+             * 经过上述的flip操作，我们的缓存区的position和limit（limit的变化取决于这次读取内容的长度，在此案例中，因为avengerEug长度本身就是10，因此
+             * limit不会变化，若想测试limit会变化的情况，可以把avengerEug改成avenger，此时limit会变化成7）发生了变化，position和limit之间的内容
+             * 就是当前循环读取到的内容。因此我们直接将缓存区参数传入到outChannel的write方法即可，
+             * write方法内部会取position到limit之前的内容写入到文件中去
+             */
             outChannel.write(byteBuffer);
-            // 切换成写模式
+
+            /**
+             * 切换成写模式  --> 一定要这么做
+             * 假设不这么做，此时的position为10，
+             * 也就是说下一次读取是从下标为10的位置开始的。
+             * 当进行下一次循环时，
+             * inChannel.read(byteBuffer)操作发现position和limit的值相等了，即无法再继续往
+             * 缓存区写数据，则直接返回原缓存区，而此时缓存区的内容还是"avengerEug"。
+             * 经过了flip操作后，下一次写文件操作就还是写"avengerEug"这段话，于是，
+             * 就这样死循环了。
+             * 因此，程序运行的结果就是D:\tmp\demo.txt文件内容全是avengerEug这段话
+             *
+             * 如果切换成了写模式的话，缓存区的position变成了0
+             * 即下一次读文件内容时，是从头（position = 0的位置）开始的，而不是从position下标为10的位置上开始读取的
+             *
+             */
             byteBuffer.clear();
         }
     }
