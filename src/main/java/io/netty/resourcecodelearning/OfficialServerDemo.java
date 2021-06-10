82d592f92ed9d0bb109cf51198b0004902c4e0e3
avengerEug <eugenesumarry@163.com>
2021-06-10 15:28:45 +0800

    netty服务端serverSocketChannel启动源码分析注释

--- src/main/java/io/netty/resourcecodelearning/OfficialServerDemo.java
+++ src/main/java/io/netty/resourcecodelearning/OfficialServerDemo.java
@@ -11,15 +11,6 @@ import io.netty.funcdemo.base.handler.NettyServerHandler;
 
 /**
  * 以一个简单的官网demo来阅读netty服务端的源码
- *
- * 记录一个问题：
- *
- *public NioServerSocketChannel(){
-      this(newSocket(DEFAULT_SELECTOR_PROVIDER));
-  }
- *
- * 这种无参构造方法调用有参构造方法，最终会有几个对象产生呢？  --> 一个
- *
  */
 public class OfficialServerDemo {
 
@@ -85,20 +76,88 @@ public class OfficialServerDemo {
             /**
              * bind方法，其地位类似于spring的refresh方法，真正的开始服务和绑定端口
              * 1、{@link AbstractBootstrap#initAndRegister()} 方法初始化了channel（利用了channelFactory内部维护的无参构造器对象创建出来了一个NioServerSocketChannel实例），
-             *   因此，我们需要去看下NioServerSocketChannel的构造方法做了哪些事情。
-             *   其最终会调用到{@link NioServerSocketChannel#NioServerSocketChannel(java.nio.channels.ServerSocketChannel)}构造方法，
+             *   因此，我们需要去看下NioServerSocketChannel的构造方法{@link NioServerSocketChannel#NioServerSocketChannel()}做了哪些事情。
+             *   无参构造方法主要是构建了一个ServerSocketChannel对象，然后传递给带参构造方法{@link NioServerSocketChannel#NioServerSocketChannel(java.nio.channels.ServerSocketChannel)}构造方法，
              *   此构造方法一共有两段代码：
              *   1.1、super(null, channel, SelectionKey.OP_ACCEPT);
-             *      其内部包含了nio服务器的一系列信息，在NioServerSocketChannel对象的构建过程中做了如下几件事：
-             *      1.1.1、使用java nio创建open了一个serverSocketChannel，获取到了一个channel
-             *      1.1.2、内部维护了SelectionKey.OP_ACCEPT事件的属性，目前还没与channel绑定起来（后续绑定端口后再拿这个属性注册到selector中去）
-             *      1.1.3、父类{@link AbstractNioChannel#AbstractNioChannel(io.netty.channel.Channel, java.nio.channels.SelectableChannel, int)}配置了channel为非阻塞模式，
-             *             同时还初始化了内部一系列属性的初始值，eg：{@link AbstractChannel#unsafe} {@link io.netty.channel.AbstractChannel#pipeline}。
-             *             其中所谓的pipeline就是我们后续入站、出站操作会经过的pipeline，内部是一个ChannelHandlerContext类型的双向链表（默认一个head和tail）.
+             *      调用父类的构造方法，并且在父类又调用父类的父类的构造方法。这块逻辑总共做了如下几件事：
+             *      1.1.1、为当前channel初始化属于自己的Unsafe（Netty自己开发的）类、pipeline(这里的pipeline目前只有head和tail)。{@link AbstractChannel#AbstractChannel(io.netty.channel.Channel)}
+             *
+             *      1.1.2、为当前channel填充serverSocketChannel、维护感兴趣的事件（此处为OP_ACCEPT事件，注意：此处还没有和serverSocketChannel进行绑定）、配置serverSocketChannel为非阻塞。{@link AbstractNioChannel#AbstractNioChannel(io.netty.channel.Channel, java.nio.channels.SelectableChannel, int)}
              *   1.2、config = new NioServerSocketChannelConfig(this, javaChannel().socket());
-             *      其内部主要作用为：维护了NioServerSocketChannel的配置，内部维护了一个socket
-             * 2、{@link AbstractBootstrap#initAndRegister()} 方法还执行了{@link AbstractBootstrap#init(io.netty.channel.Channel)}方法，其内部主要执行了
-             *    {@link ServerBootstrap#init(io.netty.channel.Channel)}方法，在pipeline的head和tail之间添加一个ChannelHandlerContext
+             *      其内部主要作用为：维护了NioServerSocketChannel的配置, 包含刚刚创建的serverSocketChannel的socket对象、channel对象
+             *      也就是说config内部包含了channel，channel属于config的一个子集
+             *
+             * 2、此外，{@link AbstractBootstrap#initAndRegister()} 方法还执行了{@link AbstractBootstrap#init(io.netty.channel.Channel)}方法，
+             *    该方法的作用为：
+             *    将serverBootstrap中配置的
+             *    childGroup（{@link ServerBootstrap#group(io.netty.channel.EventLoopGroup, io.netty.channel.EventLoopGroup) 方法中指定的workGroup}）、
+             *    childHandler（{@link ServerBootstrap#childHandler 方法中添加的ChannelInitializer}）、
+             *    等配置信息绑定到内部构建的ChannelInitializer（本质上也是一个channelHandler）中去。
+             *    最终将这个ChannelInitializer添加到serverSocketChannel的pipeline中去，等待触发。
+             *
+             *    而这个内部构建的ChannelInitializer（我们称它为 ciA），只重写了initChannel方法，其大致逻辑为：
+             *             @Override
+             *             public void initChannel(final Channel ch) {
+             *                 final ChannelPipeline pipeline = ch.pipeline();
+             *                 ChannelHandler handler = config.handler();
+             *                 if (handler != null) {
+             *                     pipeline.addLast(handler);
+             *                 }
+             *
+             *                 ch.eventLoop().execute(new Runnable() {
+             *                     @Override
+             *                     public void run() {
+             *                         // 我们称它为 ciAInner
+             *                         pipeline.addLast(new ServerBootstrapAcceptor(
+             *                                 ch, currentChildGroup, currentChildHandler, currentChildOptions, currentChildAttrs));
+             *                     }
+             *                 });
+             *             }
+             *    在这段代码中，ch为serverSocketChannel，
+             *    config为当前serverSocketChannel中的config，
+             *    也就是说，当执行到ciA的initChannel方法后，
+             *    会往serverSocketChannel的线程池中提交一个任务，而这个任务的执行逻辑就是
+             *    往serverSocketChannel的pipeline中添加ciAInner，
+             *    而这个ciAInner中维护了我们当初在serverBootstrap中配置的childGroup、childHandler的信息。
+             *    也就是说，当执行到ciA的initChannel方法后，内部就会执行ciAInner，在ciAInner内部，可能就会执行到我们在
+             *    serverBootstrap中配置的childGroup、childHandler了。
+             *    那这个方法什么时候被执行呢？ 后面在做分析，目前现在只需要知道，serverSocketChannel
+             *    的pipeline有三个channelHandler了，分别为head、tail和ciA
+             *
+             * 3、随后，在{@link AbstractBootstrap#initAndRegister()} 方法还执行了下面这段代码：
+             *    ChannelFuture regFuture = config().group().register(channel);
+             *    这里的前半段config().group()拿到的是serverBootstrap中配置的bossGroup对应的NioEventLoopGroup
+             *    因此，最终调用的是NioEventLoopGroup的register方法，其中，参数channel为NioServerSocketChannel。
+             *    大致的调用链路为：
+             *    io.netty.channel.SingleThreadEventLoop#register(io.netty.channel.Channel)
+             *      -> io.netty.channel.SingleThreadEventLoop#register(io.netty.channel.ChannelPromise)
+             *        -> io.netty.channel.AbstractChannel.AbstractUnsafe#register(io.netty.channel.EventLoop, io.netty.channel.ChannelPromise)
+             *          当调用到AbstractUnsafe的register方法后，内部会启动一个线程调用register0方法：io.netty.channel.AbstractChannel.AbstractUnsafe#register0(io.netty.channel.ChannelPromise)
+             *          在这个方法中，主要就是做注册的事情了。在初始化channel的时候，我们获取了ServerSocketChannel，也配置成了非阻塞，也有了感兴趣的事件（OP_ACCEPT）和selector，
+             *          但是却始终没有将他们注册到selector中去。这一步，就是要注册到selector中去，同时要回调pipeline中一系列的channelHandler。
+             *          3.1、在io.netty.channel.nio.AbstractNioChannel#doRegister()方法中，有一个死循环，解除循环的条件就是serverSocketChannel注册到selector了。
+             *          因此，此方法的主要目的就是将serverSocketChannel注册到selector中去
+             *          3.2、注册完了之后会执行下面这段代码：
+             *             pipeline.invokeHandlerAddedIfNeeded();
+             *          这里的主要目的就是：netty想要触发register操作之后的回调，但是呢，由于之前没有将对应的钩子函数给绑定好，
+             *          因此，此操作就是绑定一些钩子函数，而这里所说的钩子函数就是pipeline中的channelHandler。当然，方法名中
+             *          有IfNeeded的字眼，这就表示，如果需要的话，则添加，这相当于是一种扩展吧，防止一些额外逻辑。
+             *          但是在此处，如果channelHandler被添加了，则会回调ChannelInitializer类型的channelHandler中的handlerAdded方法，
+             *          而在此方法内部会回调它的initChannel方法。 还记得上面总结的ciA channelHandler吗，就是在此处被调用的。
+             *          所以，执行完此方法后，serverSocketChannel内部的pipeline中就多了一个叫ServerBootstrapAcceptor的channelHandler了。
+             *          3.3、执行完invokeHandlerAddedIfNeeded方法后，就是要在register操作成功后要触发的方法了。
+             *          而netty使用的是责任链设计模式来完成这个功能的，其实也可以用发布订阅的模式（类似于spring回调后置处理器）
+             *
+             * 至此，服务端的netty启动程序的主逻辑就是这样啦，有几个比较关键的点：
+             * 1、每一个channel内部都会包含一个pipeline
+             * 2、每一个pipeline初始化的时候都只有一个head和tail
+             * 3、serverSocketChannel注册到selector后会触发一个add的钩子函数，此钩子函数执行完毕后，会往pipeline中添加
+             *    一个叫ServerBootstrapAcceptor的channelHandler，此handler内维护了workgroup的相关信息，后续将由它来
+             *    处理客户端的非OP_ACCEPT的所有请求
+             * 4、netty每执行完一系列操作后，都会触发一系列的钩子函数。相当于在netty交互的生命周期中都有触发一些钩子函数。
+             *    这种设计思想还是值得学习的，扩展性非常高。
+             *
              *
              *
              */
--- 自己开发的core包如何找到适合集成方的版本/README.md
+++ 自己开发的core包如何找到适合集成方的版本/README.md
@@ -1,26 +1,32 @@
-#  干货：业务侧集成自己开发的starter，如何定位合适的依赖包版本
+#  干货：自己开发了一个starter，如何轻松嵌入到使用不同版本的springboot框架的对接方
 
 ## 背景
 
 * 最近应公司需求，开发了一个公司内部共用的二方库包。这里科普下：什么叫一方库，二方库，三方库。在阿里巴巴出品的 **泰山版《Java开发手册》 `附 2：专有名词解释`**章节中有提到
 
-  1. 一方库：本工程内部子项目模块依赖的库（jar 包）
+* 一方库：本工程内部子项目模块依赖的库（jar 包）
 
-     > 依赖本工程内部的**common**模块，里面通常会定义一些所有模块都会用到的一些基础功能，比如**全局异常码、分布式锁、统一响应实体等等**
+> 依赖本工程内部的**common**模块，里面通常会定义一些所有模块都会用到的一些基础功能，比如**全局异常码、分布式锁、统一响应实体等等**
 
-  2. 二方库：公司内部发布到中央仓库，可供公司内部其它应用依赖的库（jar 包）
+* 二方库：公司内部发布到中央仓库，可供公司内部其它应用依赖的库（jar 包）
 
-     > 在互联网公司通常都是分事业部的，然后会存在一个叫**基础部门/基础中心**的事业部，这个部门可能会提供一些公司的基本类库，eg：mq相关的统一的amqp操作、rpc相关的统一的dubbo操作等等。最终这些都会以jar包的形式推送到maven仓库（公司内部需要搭建专有的maven仓库）。现在是spring boot的时代，如果公司用的是springboot这一套体系的话，这些jar包通常是以starter的方式提供，再利用springboot的自动装配机制，其他事业部可以很方便的对接。
+> 在互联网公司通常都是分事业部的，然后会存在一个叫**基础部门/基础中心**的事业部，这个部门可能会提供一些公司的基本类库，eg：mq相关的统一的amqp操作、rpc相关的统一的dubbo操作等等。最终这些都会以jar包的形式推送到maven仓库（公司内部需要搭建专有的maven仓库）。现在是spring boot的时代，如果公司用的是springboot这一套体系的话，这些jar包通常是以starter的方式提供，再利用springboot的自动装配机制，其他事业部可以很方便的对接。
 
-  3. 三方库：公司之外的开源库（jar 包）
+* 三方库：公司之外的开源库（jar 包）
 
-     > 常见的springboot、mybatis
+> 常见的springboot、mybatis
 
-  由于公司内部使用的是springboot相关的技术栈，因此，我开发的二方库包是以starter的方式提供的。整个对接流程只需要两步：第一，需要将二方库包推送至maven仓库。第二：其他事业部只需要添加二方库包的maven坐标。这里会有一些小细节：二方库包是一个项目工程，也会依赖一些二方库、三方库的情况。通常，二方库内部依赖的jar包的scope都会设置为provider（默认系统已经提供，不将二方库包内部依赖的jar包与对接方的项目耦合），但是不乏会出现对接方无添加过相关依赖，因此**需要添加符合对接方系统版本的依赖**。接下来，将以一个springboot引入spring-kafka的案例来理解如何找到合适的spring-kafka版本。
+由于公司内部使用的是springboot相关的技术栈，因此，我开发的二方库包是以starter的方式提供的。整个对接流程只需要两步：
+
+> 1、需要将二方库包推送至maven仓库
+>
+> 2、其他事业部只需要添加二方库包的maven坐标
+
+这里会有一些小细节：二方库包是一个项目工程，也会依赖一些二方库、三方库的情况。通常，二方库内部依赖的jar包的scope都会设置为provider（**默认系统已经提供，不将二方库包内部依赖的jar包与对接方的项目耦合**），但是不乏会出现对接方无添加过相关依赖的情况，因此**需要添加符合对接方系统版本的依赖**。接下来，将以一个springboot引入spring-kafka的案例来理解如何找到合适的spring-kafka版本。
 
 ## 一、为什么maven会找不到spring-kafka的版本？
 
-* 假设这么一个场景：我开发的二方库包使用到的技术栈是springboot，内部开发的其中一个功能需要依赖**spring-kafka**的三方库。众所周知，springboot为我们提供了及其舒适的开发环境，当我们要引入springboot框架时，它内部已经定义了符合当前版本的所有三方库依赖，因此，当我们需要引入spring-kafka模块时，只需要在pom.xml文件中添加如下坐标即可：
+* 假设这么一个场景：我开发的二方库包使用到的技术栈是springboot，内部开发的其中一个功能需要依赖**spring-kafka**的三方库。众所周知，springboot为我们提供了及其舒适的开发环境，当我们引入springboot框架后，它内部已经定义了符合当前版本的所有三方库依赖。因此，假设我们要引入spring-kafka模块，只需要在pom.xml文件中添加如下坐标即可：
 
   ```xml
   <parent>
@@ -39,7 +45,7 @@
   </dependencies>
   ```
 
-  在上述引入spring-kafka 模块时，我们没有指定version也可以使用。这是因为spring-boot-starter-parent内部的dependencyManagement中定义了spring-kafka的依赖，作为子模块，只需要引入即可，版本号会默认继承父工程中的定义。如果我们使用的是**springboot 1.4.1.RELEASE**版本，这个时候，再使用同样的方式来引入spring-kafka的话，你会发现，maven找不到spring-kafka的版本：
+  在上述引入spring-kafka 模块时，我们没有指定version也可以使用。这是因为spring-boot-starter-parent内部的dependencyManagement中定义了spring-kafka的依赖，作为子模块，只需要引入即可，版本号会默认继承父工程中的定义。如果对接方使用的是**springboot 1.4.1.RELEASE**版本，这个时候，再使用同样的方式来引入spring-kafka的话，你会发现，maven找不到spring-kafka的版本：
 
   ```xml
   <parent>
@@ -62,13 +68,13 @@
 
 ## 二、如何找到springboot 1.4.1.RELEASE内部的依赖
 
-* 其中最简单的一种方法就是在idea中找到对应的pom文件定义，这是一种方式。但是作为一个二方库提供者，后续可能会经常要处理因业务侧springboot版本不同而导致无法引入spring-kafka的问题。难道我们要本地每次修改springboot的版本，通过maven下载后再使用idea查看吗？这显然有点麻烦。
+* 其中最简单的一种方法就是在**idea中找到对应的pom文件定义**，这是一种方式。但是作为一个二方库提供者，后续可能会经常要处理因业务侧springboot版本不同而导致无法引入spring-kafka的问题。难道我们要本地每次修改maven中springboot的版本，通过maven下载后再使用idea查看吗？这显然有点麻烦。
 
 * 还有一种做法是找官方springboot 1.4.1.RELEASE的文档，但连maven依赖中都没有涉及kafka，大概率是此版本没有集成kafka。
 
-* 另外一种做法就是：参考此链接[https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-dependencies/](https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-dependencies/)，在此链接中，是springboot的所有版本的依赖，现在我们要看1.4.1.RELEASE版本的springboot内部依赖了哪些类库，我们可以这么定位：
+* 另外一种做法就是：参考此链接[https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-dependencies/](https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-dependencies/)。在此链接中，是springboot的所有版本的依赖，现在我们要看1.4.1.RELEASE版本的springboot内部依赖了哪些类库，我们可以这么定位：
 
-  1. 定义版本
+  1. 定位版本
 
      ![定位版本.png](定位版本.png)
 
@@ -76,11 +82,11 @@
 
      ![查看pom.png](查看pom.png)
 
-  3. 善于搜索
+  3. 善于搜索（搜索spring-kafka）
 
      ![搜索.png](搜索.png)
 
-     可以看到，在springboot 1.4.1.RELEASE版本中内部并没有依赖spring-kafka。此时该怎么办呢？**继续往后搜索**，最终你会发现，在**1.5.0.RELEASE**版本中内部开始依赖了spring-kafka。因此，我们可以尝试，引入查找出来的版本**1.1.2.RELEASE**
+     可以看到，在springboot 1.4.1.RELEASE版本中内部并没有依赖spring-kafka。此时该怎么办呢？**将springboot版本升级，继续往后搜索**，最终你会发现，在**1.5.0.RELEASE**版本中内部开始依赖了spring-kafka。因此，我们可以尝试，在maven中引入查找出来的版本**1.1.2.RELEASE**
 
      ![spring-kafka.png](spring-kafka.png)
 
@@ -94,7 +100,9 @@
      </dependency>
      ```
 
-     在这种场景下，引入**1.1.2.RELEASE**版本可以解决问题。也许这种方式在其他场景不适用，但这确实是一种方式，我们可以通过https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-dependencies/连接找到每个springboot版本内部依赖的三方库，这比我们在idea中修改版本号再下载对应的版本方便的多。
+  在这种场景下，引入**1.1.2.RELEASE**版本可以解决问题，对接方的系统也能正常工作。
+  
+  也许这种方式在其他场景不适用，但这确实是一种方式，我们可以通过https://repo1.maven.org/maven2/org/springframework/boot/spring-boot-dependencies/链接找到每个springboot版本内部依赖的三方库，这比我们在idea中修改版本号再下载对应的版本方便的多。
 
 ## 三、总结
 
