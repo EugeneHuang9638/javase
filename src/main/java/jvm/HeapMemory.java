bbc8bd6a627f49919924b9c5edf4ce787d514bb3
avengerEug <eugenesumarry@163.com>
2023-12-29 17:52:47 +0800

    新增建造者设计模式总结 & jvm堆内存测试

--- src/main/java/jvm/HeapMemory.java
+++ src/main/java/jvm/HeapMemory.java
@@ -2,6 +2,7 @@ package jvm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class HeapMemory {
 
@@ -9,15 +10,25 @@ public class HeapMemory {
     private char[] chars = new char[1024 * 100];
 
     public static void main(String[] args) throws InterruptedException {
+        // 使用java -D命令设置的系统参数可以用System.getProperty(String key); api来获取
+        Properties properties = System.getProperties();
+        System.out.println(properties.size());
+        properties.forEach((a, b) -> System.out.println(a + " >> " + b));
+        for (String str : args) {
+            System.out.println(str);
+        }
 
-        System.out.println("Starting");
+
+        System.out.println("=====================Starting=====================");
         List<HeapMemory> list = new ArrayList();
-        for (int i = 0; i < 1000; i++) {
-            // 循环1000次，每隔100毫秒往list添加一个DumpMemory对象， 最后使用jconsole来定位main线程，来查看堆内存的变化
+        for (;;) {
+            // 循环1000次，每隔100毫秒往list添加一个DumpMemory对象， 最后使用jconsole或jvisualvm来定位main线程，来查看堆内存的变化
             Thread.sleep(100);
             list.add(new HeapMemory());
         }
-        System.out.println("end");
+        // 手动触发fullgc
+//        System.gc();
+//        System.out.println("end");
     }
 
 }
